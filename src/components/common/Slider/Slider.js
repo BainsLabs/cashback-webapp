bf64fda007ae5d94f19b0890ab7cf15b1218b72c
akkifullstack <saymefit@gmail.com>
2019-07-13 23:34:02 +0530

    slider implemented

---
+++ src/components/common/Slider/Slider.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Slider from "react-slick";
+import { Waypoint } from "react-waypoint";
+
+const CommonSlider = ({
+  children,
+  lazyloading,
+  infiniteSetting,
+  speedSetting,
+  slidesToScrollSetting,
+  slidesToShowSetting,
+  initialSlideSetting
+}) => {
+  const settings = {
+    lazyLoad: lazyloading,
+    infinite: infiniteSetting,
+    speed: speedSetting,
+    slidesToShow: slidesToShowSetting,
+    slidesToScroll: slidesToScrollSetting,
+    initialSlide: initialSlideSetting,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+  return (
+    <>
+      <Waypoint>
+        <Slider {...settings}>{children}</Slider>
+      </Waypoint>
+    </>
+  );
+};
+
+export default CommonSlider;
