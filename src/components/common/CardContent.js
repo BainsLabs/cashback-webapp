91ccdab34c507dd6abc40c60064616ebba8abdc7
Akshay Sharma <akshay.sharma@bainslabs.com>
2019-07-31 17:28:04 +0530

    login signup fix

--- src/components/common/CardContent.js
+++ src/components/common/CardContent.js
@@ -1,52 +1,36 @@
-import React from "react";
-import { Row, Col } from "react-bootstrap";
-// import classNames from 'classnames';
-import styled from "styled-components";
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 
-const StyledDiv = styled.div`
-  transition: 0.5s ease;
-  opacity: 0;
-  position: absolute;
-  top: 10px;
-  left: 2px;
-  transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  text-align: center;
-  &:hover {
-    display: inline;
-    opacity: 1;
-  }
-`;
-
-export default props => {
+export default (props) => {
   const { brandLogo, data } = props;
   return (
     <>
       <div className="card__container">
-        <Row>
-          <Col lg={4}>
-            <p>{data.cahsback}</p>
-            <p>{data.offerType}</p>
+        <Row className="hidden-content">
+          <Col lg={5} className="no-padding">
+            <p className="cashback-percentage">{data.cahsback}</p>
+            <p className="cashback-offer">{data.offerType}</p>
           </Col>
           <Col lg={4}>
-            <p>{data.tlc}</p>
+            <p className="cashback-TLC">{data.tlc}</p>
           </Col>
-          <Col lg={4}>
-            <img
-              src={brandLogo}
-              alt="KFC-LOGO"
-              className="card__content-image"
-            />
+          <Col lg={3}>
+            <img src={brandLogo} alt="KFC-LOGO" className="card__content-image" />
+          </Col>
+        </Row>
+        <Row className="hover-content">
+          <Col lg={12} className="no-padding">
+            <p className="cashback-percentage">{data.cahsback}</p>
+            <p className="cashback-offer">{data.offerType}</p>
+          </Col>
+          <Col lg={12}>
+            <p className="cashback-TLC">{data.tlc}</p>
+          </Col>
+          <Col lg={12}>
+            <img src={brandLogo} alt="KFC-LOGO" className="card__content-image" />
           </Col>
         </Row>
       </div>
-      <StyledDiv>
-        <Row>
-          {/* <Col>
-            <img src={brandLogo} alt="brandLogo" />
-          </Col> */}
-        </Row>
-      </StyledDiv>
     </>
   );
 };
