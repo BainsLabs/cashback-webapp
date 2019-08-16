3d0e244ae7d88383d80f58a0e7a8c046367f6140
akkifullstack <saymefit@gmail.com>
2019-08-16 18:31:15 +0530

    change in wallet page

--- src/components/mywallet/index.js
+++ src/components/mywallet/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import Input from 'components/common/inputField';
-import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
-import Banner from 'components/common/Banner';
-import BannerImage from 'static/images/cashback/wallet.png';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Input from "components/common/inputField";
+import { FormattedMessage, injectIntl, intlShape } from "react-intl";
+import Banner from "components/common/Banner";
+import BannerImage from "static/images/cashback/wallet.png";
 
-const MyWallet = (props) => {
+const MyWallet = props => {
   const { intl } = props;
   return (
     <>
-      <Banner imgSrc={BannerImage} className="banner-padding" textPosition="center">
+      <Banner
+        imgSrc={BannerImage}
+        className="banner-padding"
+        textPosition="center"
+      >
         <h2 className="vipbanner-text">
           <FormattedMessage id="data.mywallet" />
         </h2>
@@ -20,8 +24,7 @@ const MyWallet = (props) => {
             <Container className="profile__container">
               <h3 className="text-center">
                 <FormattedMessage id="data.request" />
-                {' '}
-TLC
+                &nbsp; TLC&nbsp;
                 <FormattedMessage id="data.payout" />
               </h3>
               <Container className="membership__box">
@@ -29,37 +32,41 @@ TLC
                   <Col md={8} className="mywallet-form">
                     <Input
                       label={intl.formatMessage({
-                        id: 'data.selectpaymentmethod',
+                        id: "data.selectpaymentmethod"
                       })}
                       type="text"
-                      placeholder={intl.formatMessage({ id: 'data.tlcwallet' })}
+                      placeholder={intl.formatMessage({ id: "data.tlcwallet" })}
                       labelClass="wallet"
                     />
                     <Input
-                      label={intl.formatMessage({ id: 'data.enteramount' })}
+                      label={intl.formatMessage({ id: "data.enteramount" })}
                       type="text"
-                      placeholder={intl.formatMessage({ id: 'data.amount' })}
+                      placeholder={intl.formatMessage({ id: "data.amount" })}
                       labelClass="wallet"
                     />
-                    <button align="center" className="text-uppercase" style={{ fontSize: '.8rem' }}>
+                    <button
+                      align="center"
+                      className="text-uppercase"
+                      style={{ fontSize: ".8rem" }}
+                    >
                       <FormattedMessage id="data.submitrequest" />
                     </button>
                   </Col>
                   <Col md={4}>
                     <div className="eclips text-center">
-                      <p style={{ fontSize: '1.5rem' }}>0</p>
+                      <p style={{ fontSize: "1.5rem" }}>0</p>
                       <p>
                         <FormattedMessage id="data.withdrawn" />
                       </p>
                     </div>
                     <div className="eclips text-center">
-                      <p style={{ fontSize: '1.5rem' }}>0</p>
+                      <p style={{ fontSize: "1.5rem" }}>0</p>
                       <p>
                         <FormattedMessage id="data.available" />
                       </p>
                     </div>
                     <div className="eclips text-center">
-                      <p style={{ fontSize: '1.5rem' }}>5</p>
+                      <p style={{ fontSize: "1.5rem" }}>5</p>
                       <p>
                         <FormattedMessage id="data.headermyearningspending" />
                       </p>
@@ -73,27 +80,26 @@ TLC
             <Container className="profile__container">
               <h3 className="text-center">
                 <FormattedMessage id="data.request" />
-                {' '}
-$
+                &nbsp; $&nbsp;
                 <FormattedMessage id="data.payout" />
               </h3>
               <Container className="membership__box">
                 <Row>
                   <Col md={4}>
                     <div className="eclips text-center">
-                      <p style={{ fontSize: '1.5rem' }}>$0</p>
+                      <p style={{ fontSize: "1.5rem" }}>$0</p>
                       <p>
                         <FormattedMessage id="data.withdrawn" />
                       </p>
                     </div>
                     <div className="eclips text-center">
-                      <p style={{ fontSize: '1.5rem' }}>$0</p>
+                      <p style={{ fontSize: "1.5rem" }}>$0</p>
                       <p>
                         <FormattedMessage id="data.available" />
                       </p>
                     </div>
                     <div className="eclips text-center">
-                      <p style={{ fontSize: '1.5rem' }}>$5</p>
+                      <p style={{ fontSize: "1.5rem" }}>$5</p>
                       <p>
                         <FormattedMessage id="data.headermyearningspending" />
                       </p>
@@ -102,21 +108,25 @@ $
                   <Col md={8} className="mywallet-form">
                     <Input
                       label={intl.formatMessage({
-                        id: 'data.selectpaymentmethod',
+                        id: "data.selectpaymentmethod"
                       })}
                       type="text"
                       placeholder={intl.formatMessage({
-                        id: 'data.bitcoinpaypal',
+                        id: "data.bitcoinpaypal"
                       })}
                       labelClass="wallet"
                     />
                     <Input
-                      label={intl.formatMessage({ id: 'data.enteramount' })}
+                      label={intl.formatMessage({ id: "data.enteramount" })}
                       type="text"
-                      placeholder={intl.formatMessage({ id: 'data.amount' })}
+                      placeholder={intl.formatMessage({ id: "data.amount" })}
                       labelClass="wallet"
                     />
-                    <button align="center" className="text-uppercase" style={{ fontSize: '.8rem' }}>
+                    <button
+                      align="center"
+                      className="text-uppercase"
+                      style={{ fontSize: ".8rem" }}
+                    >
                       <FormattedMessage id="data.submitrequest" />
                     </button>
                   </Col>
@@ -131,7 +141,7 @@ $
 };
 
 MyWallet.propTypes = {
-  intl: intlShape.isRequired,
+  intl: intlShape.isRequired
 };
 
 export default injectIntl(MyWallet);
