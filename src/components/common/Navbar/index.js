c483a61c394b37ffd32d43c71e6158165e37efe8
Akshay Sharma <akshay.sharma@bainslabs.com>
2019-09-02 16:29:20 +0530

    navbar fixed

--- src/components/common/Navbar/index.js
+++ src/components/common/Navbar/index.js
@@ -1,19 +1,14 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable no-shadow */
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { sidebarState } from 'redux/actions/sidebarActions';
-import {
-  Navbar, Container, Nav, NavDropdown,
-} from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
 import { modalState } from 'redux/actions/modalActions';
-import DropdownComponent from 'components/common/DropDown';
-import MobileNavBar from './Mobile/MobileNavbar';
 import TopNavbar from './TopNavbar';
-import secondaryNavbar from './secondaryNavbar';
 
 const style = {
   buttonBackground: {
@@ -25,6 +20,7 @@ const style = {
 };
 
 const NavBar = (props) => {
+  const [navbarState, setNavbarState] = useState(false);
   // eslint-disable-next-line no-unused-vars
   const {
     user, sidebarState, sidebar, modalState, intl,
@@ -38,30 +34,37 @@ const NavBar = (props) => {
       </div> */}
       <TopNavbar />
       {/* <secondaryNavbar /> */}
-      <Navbar className="navbar" expand="lg">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar className="navbar" expand="lg" expanded={navbarState}>
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          onClick={() => setNavbarState(!navbarState)}
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <div
             className={language === 'en-US' ? 'container secondary__nav' : 'container chinese__nav'}
           >
             <Nav className="navbar__list">
               <Nav.Item>
-                <Link to="/categories">
+                <Link to="/categories" onClick={() => setNavbarState(!navbarState)}>
                   <FormattedMessage id="data.menuHPshopcategories" />
                 </Link>
               </Nav.Item>
               <Nav.Item>
-                <Link to="/how-cashback-works" className="text-capitalize">
+                <Link
+                  to="/how-cashback-works"
+                  onClick={() => setNavbarState(!navbarState)}
+                  className="text-capitalize"
+                >
                   <FormattedMessage id="data.menuHPcashbackworks" />
                 </Link>
               </Nav.Item>
               <Nav.Item>
-                <Link to="/refer-friend">
+                <Link to="/refer-friend" onClick={() => setNavbarState(!navbarState)}>
                   <FormattedMessage id="data.menuHPcapsrefer" />
                 </Link>
               </Nav.Item>
               <Nav.Item>
-                <Link to="/vip-benefits">
+                <Link to="/vip-benefits" onClick={() => setNavbarState(!navbarState)}>
                   <FormattedMessage id="data.vipbenefits" />
                 </Link>
               </Nav.Item>
